c3d4eac319c9e8194dafb8b6c7c557d6ac52837c
kajisan0415 <agonagakajisan0415@gmail.com>
2021-04-30 07:04:57 +0000

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,17 +12,17 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
-  
+
   test "email should be present" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  
+
  test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
@@ -31,20 +31,20 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
   end
-  
+
    test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
-  
+
     test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -54,5 +54,5 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  
+
 end
